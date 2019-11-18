cf34a15ff473f9baac4547b455b1673e45dc1c39
saroj-sudo <saroj.basnet1237@gmail.com>
2019-11-18 20:00:14 +0545

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.red[200],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            Text('')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
